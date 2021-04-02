23dc71724ce155825c27c28e3016631e9c4af0e4
githubGaboDotCom <completamentesud@gmail.com>
2021-04-02 02:42:07 -0600

    Working on the main page, adding background video and the stopwatch.

--- stopwatchForMainPage.js
+++ stopwatchForMainPage.js
@@ -1,1 +1,35 @@
-document.getElementById("stopwatch").innerHTML = 5 + 6;
+function stopwatch() {
+
+    var second = 0, minute = 0, hour = 0, day = 0; 
+    setInterval(function seconds()
+    {
+        document.getElementById("stopwatch").innerHTML = day + "d " + hour + "h " + minute + "m " + second + "s ";
+        
+        if (second <= 59)
+        {
+            second += 1;
+        }
+
+
+        if (second > 59)
+        {
+            minute += 1;
+            second = 0;
+
+        }
+        
+        if (minute > 59)
+        {
+            hour += 1;
+            minute = 0;
+        }
+        
+        if (hour > 23)
+        {
+            day += 1;
+            hour = 0; 
+        }
+        
+
+    } ,1000);
+}
