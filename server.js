c608fa803bc1306007bf3f5e89ea0dae84ddc2b0
githubGaboDotCom <completamentesud@gmail.com>
2022-08-17 14:55:50 -0600

    Testing the Heroku connection

--- server.js
+++ server.js
@@ -4,9 +4,9 @@ var port = 5000;
 
 myApp.use(express.static('public'));
 
-myApp.get('/', (res, req) => {
-    console.log("Node.js App is now being hosted on Heroku!");
-});
+// myApp.get('/', (res, req) => {
+//     console.log("Node.js App is now being hosted on Heroku!");
+// });
 
 myApp.listen(process.env.PORT || port, () => {
     console.log("The server is now actively listening on port 5000.")
