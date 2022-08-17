da064399911e3cec8a33bb18f8f3d039e7a67a8a
githubGaboDotCom <completamentesud@gmail.com>
2022-08-16 21:43:48 -0600

    Static files have been put into the public folder so they can be accessed and displayed by the node.js app.

--- server.js
+++ server.js
@@ -1,1 +1,8 @@
-console.log("Testing output to the console");
+var express = require('express');
+var myApp = express();
+
+myApp.use(express.static('public'));
+
+myApp.listen('5000', () => {
+    console.log("The server is now actively listening on port 5000.")
+});
