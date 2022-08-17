704969f2a9fb9798d6a5d7bee264553d82a88dd1
githubGaboDotCom <completamentesud@gmail.com>
2022-08-17 14:53:16 -0600

    Testing the Heroku connection with my Node.js app. There's ane error and it's not allowing to display my website

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 var express = require('express');
 var myApp = express();
+var port = 5000;
 
 myApp.use(express.static('public'));
 
-myApp.listen('5000', () => {
+myApp.get('/', (res, req) => {
+    console.log("Node.js App is now being hosted on Heroku!");
+});
+
+myApp.listen(process.env.PORT || port, () => {
     console.log("The server is now actively listening on port 5000.")
 });
